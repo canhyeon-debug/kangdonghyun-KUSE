6243f65f822a1f740d1bbbed9214df09537c0515
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-07 18:27:09 +0900

    baekjoon 27110 solution

---
+++ baekjoon27110.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+A, B, C = map(int, input(). split())
+
+if A < N:
+    A = A
+else:
+    A = N
+
+if B < N:
+    B = B
+else:
+    B = N
+
+if C < N:
+    C = C
+else:
+    C = N
+
+print(A + B + C)
