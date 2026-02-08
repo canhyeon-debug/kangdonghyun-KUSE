8f5885899439cfefe3658c46abac597cc8c10262
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-08 12:08:49 +0900

    important!_baekjoon 3058 solution

--- baekjoon/baekjoon3058.py
+++ baekjoon/baekjoon3058.py
@@ -4,9 +4,11 @@ a = int(input())
 for i in range(a):
     num = list(map(int, input().split()))
     results = []
+    #리스트를 어디에 두느냐에 따라 초기화 여부가 달라진다!
+
     for j in num:
         if j%2 == 0:
             results.append(j)
-            
 
     print(sum(results), min(results))
+    #sum함수는 괄호 안에 있는 요소들은 합쳐주는 함수
