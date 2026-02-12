25ed1f339ecc4d5e82961dafa5305b8d635629f9
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-12 23:35:00 +0900

    important!_baekjoon 19532 solution

--- baekjoon/baekjoon19532.py
+++ baekjoon/baekjoon19532.py
@@ -1,7 +1,9 @@
 a, b, c, d, e, f = map(int, input().split())
 
-x = ((e*c//b)-f)//((e*a//b)-d)
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
+            
+            # 이 문제는 수학 연산 문제가 아니라 완전 탐색 문제다!
 
-y = (c-(a*f//d))//(b-(a*e//d))
-
-print('{} {}'.format(x, y))
