d493a5511fb01f0db3ffaa3950ba8b375d132c7e
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-12 23:33:26 +0900

    important!_baekjoon 5613 solution

--- baekjoon/baekjoon5613.py
+++ baekjoon/baekjoon5613.py
@@ -1,5 +1,5 @@
 results = int(input())
-
+#input으로 입력받은 변수는 초기화해서 다시 사용 가능 함을 잊지말자!
 while True:
     M = str(input())
 
@@ -9,7 +9,7 @@ while True:
         A = int(input())
         
         if M == '+':
-            results += A
+            results += A 
         elif M == '-':
             results -= A
         elif M == '*':
