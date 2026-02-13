0b83e93a47ac3b9c09eb8899d941e8fe177e5f44
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-13 21:03:28 +0900

    important!_replace()_baekjoon_2941_solution

--- baekjoon/baekjoon2941.py
+++ baekjoon/baekjoon2941.py
@@ -10,6 +10,9 @@ results =  results.replace('lj', '*')
 results =  results.replace('nj', '*')
 results =  results.replace('s=', '*')
 
+#문자열을 다른 문자열로 바꾸는 함수인 replace().
+#어떤 문자열.replace('바꿀 문자열', '바뀐 문자열') = 새로운 리스트 --> 이런 식으로 사용.
+
 print(len(results))
 
 
