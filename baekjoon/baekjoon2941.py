a97e5601e966137a1cafd84702b839190d7beb73
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-02-13 20:43:22 +0900

    important!_replace()_baekjoon_2941_solution

---
+++ baekjoon/baekjoon2941.py
@@ -0,0 +1,16 @@
+alp = str(input())
+results = []
+
+results = alp.replace('c=', '*')
+results = results.replace('c-', '*')
+results = results.replace('dz=', '*')
+results =  results.replace('z=', '*')
+results = results.replace('d-', '*')
+results =  results.replace('lj', '*')
+results =  results.replace('nj', '*')
+results =  results.replace('s=', '*')
+
+print(len(results))
+
+
+ 
