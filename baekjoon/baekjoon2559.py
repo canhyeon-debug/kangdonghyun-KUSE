2628aff55b9427c593631e037f0f5fe6d54245d7
canhyeon-debug <canhyeon@kookmin.ac.kr>
2026-03-14 15:48:22 +0900

    silver3_slidingwindow_2559

---
+++ baekjoon/baekjoon2559.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+tem = list(map(int, input().split()))
+result = []
+sum = 0
+
+for i in range(K):
+    sum += tem[i]
+
+result.append(sum)
+
+for a in range(K, N):
+    sum += tem[a] - tem[a-K]
+    result.append(sum)
+
+
+print(result)
+print(max(result))
